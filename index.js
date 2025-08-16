aa3e9b4cc3a31384d62994abc5ab8b65dc1a3b44
Your Name <you@example.com>
2025-08-17 00:43:16 +0900

    202508170043

--- index.js
+++ index.js
@@ -1,59 +1,91 @@
 window.onload = function () {
 
-  let t_all = document.getElementsByName('t_all')[0];
-  let tr_all = document.getElementsByName('tr_all')[0];
-  let r_all = document.getElementsByName('r_all')[0];
+  if (document.querySelectorAll('[name="t_all"]').length > 0) {
 
-  // console.log(t_all.length);
-  // console.log(t_all.values);
+    let t_all = document.getElementsByName('t_all')[0];
+    let tr_all = document.getElementsByName('tr_all')[0];
+    let r_all = document.getElementsByName('r_all')[0];
 
-  // function inputChange() {
-  //   console.log('Change');
-  // };
+    // console.log(t_all.length);
+    // console.log(t_all.values);
 
-  t_all.addEventListener('change', function () {
+    // function inputChange() {
+    //   console.log('Change');
+    // };
 
-    var t_all_val = this.value;
+    t_all.addEventListener('change', function () {
 
-    t_s = document.querySelectorAll('[name^="t_"]');
+      var t_all_val = this.value;
 
-    // console.log(t_s.length);
+      t_s = document.querySelectorAll('[name^="t_"]');
 
-    t_s.forEach(sel => {
-      // console.log(sel.value);
-      sel.value = t_all_val;
+      // console.log(t_s.length);
+
+      t_s.forEach(sel => {
+        // console.log(sel.value);
+        sel.value = t_all_val;
+      });
+
     });
 
-  });
+    tr_all.addEventListener('change', function () {
 
-  tr_all.addEventListener('change', function () {
+      var tr_all_val = this.value;
 
-    var tr_all_val = this.value;
+      tr_s = document.querySelectorAll('[name^="tr_"]');
 
-    tr_s = document.querySelectorAll('[name^="tr_"]');
+      // console.log(t_s.length);
 
-    // console.log(t_s.length);
+      tr_s.forEach(sel => {
+        // console.log(sel.value);
+        sel.value = tr_all_val;
+      });
 
-    tr_s.forEach(sel => {
-      // console.log(sel.value);
-      sel.value = tr_all_val;
     });
 
-  });
+    r_all.addEventListener('change', function () {
 
-  r_all.addEventListener('change', function () {
+      var r_all_val = this.value;
 
-    var r_all_val = this.value;
+      r_s = document.querySelectorAll('[name^="r_"]');
 
-    r_s = document.querySelectorAll('[name^="r_"]');
+      // console.log(t_s.length);
 
-    // console.log(t_s.length);
+      r_s.forEach(sel => {
+        // console.log(sel.value);
+        sel.value = r_all_val;
+      });
 
-    r_s.forEach(sel => {
-      // console.log(sel.value);
-      sel.value = r_all_val;
     });
 
-  });
+  }
+
+  if (document.querySelectorAll('[id="u_i"]').length > 0) {
+
+    //https://jsfiddle.net/Sapphion/HcD9c/
+
+    let input = document.getElementById('u_i');
+    let output = document.getElementById('dir_name');
+
+    input.onchange = function (e) {
+
+      let files = e.target.files; // FileList
+
+      let l_file_path = [];
+      // let output_value = '';
+
+      for (var i = 0, file; file = files[i]; ++i) {
+        // for (let file in files) {
+        l_file_path.push(file.webkitRelativePath);
+        // output_value += file.webkitRelativePath + '****';
+
+      }
+
+      output.value = l_file_path.join('****');
+      
+    }
+
+  }
 
 }
+
